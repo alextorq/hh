ec6d21a64a79da2c2efbfdae60054f09361e204d
mr_Green <xxxxx_07@inbox.ru>
2020-03-15 12:40:16 +0300

    s

---
+++ server/front/src/repository/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const host = 'http://127.0.0.1:4001/';
+
+export function getCategories() {
+  return axios.get(`${host}category/list`).then((response) => Object.freeze(response.data));
+}
+
+export function getVacancies() {
+  return axios.get(`${host}vacancy/list`).then((response) => Object.freeze(response.data));
+}
+
+export function getCategoriesWithVacancies() {
+  return axios.get(`${host}category/list_with_vacancies`).then((response) => Object.freeze(response.data));
+}
+
+export function getCategoriesWithSpecialization() {
+  return axios.get(`${host}category/list_with_specialization`).then((response) => Object.freeze(response.data));
+}
