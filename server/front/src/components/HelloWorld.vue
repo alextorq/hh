250ce90eb9f63060b6c60ab8d01b9bf791fdfbda
mr_Green <xxxxx_07@inbox.ru>
2020-03-15 12:48:56 +0300

    s

--- server/front/src/components/HelloWorld.vue
+++ server/front/src/components/HelloWorld.vue
@@ -1,32 +1,74 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <el-container>
+      <el-header>
+        <el-select v-model="value" placeholder="Select">
+          <el-option
+            v-for="item in categories"
+            :key="item._id"
+            :label="item.title"
+            :value="item._id">
+          </el-option>
+        </el-select>
+      </el-header>
+      <el-main>
+        <el-tabs type="card">
+          <el-tab-pane label="Price">
+            <div style="width:80vw; position: relative;">
+              <averageSalary :info="adaptCaregoriesWithVacancies" v-if="adaptCaregoriesWithVacancies.length"/>
+            </div>
+          </el-tab-pane>
+          <el-tab-pane label="Amount">
+            <div style="width: 80vw; position: relative;">
+              <AmountVacancies :info="categories" v-if="categories.length"/>
+            </div>
+          </el-tab-pane>
+
+          <el-tab-pane label="Profession">
+            <div style="width: 80vw; position: relative;">
+              <averageSalaryByProfession :info="adaptCaregoriesWithVacancies" v-if="adaptCaregoriesWithVacancies.length"/>
+            </div>
+          </el-tab-pane>
+
+        </el-tabs>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+<script>
+  import { getVacancies, getCategories, getCategoriesWithVacancies, getCategoriesWithSpecialization } from '../repository/index';
+  import adaptProf from '../utils/midlle';
+  import averageSalary from './averageSalary.vue';
+  import AmountVacancies from "./AmountVacancies.vue";
+  import averageSalaryByProfession from './averageSalaryByProfession.vue'
 
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
-}
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        categories: [],
+        vacancies: [],
+        midlle: {},
+        adaptCaregoriesWithVacancies: [],
+        activeName: 'first',
+        value: '',
+      };
+    },
+    components: {
+      averageSalary,
+      AmountVacancies,
+      averageSalaryByProfession,
+    },
+    async mounted() {
+      this.categories = await getCategories();
+      this.vacancies = await getVacancies();
+      // console.log(await getCategoriesWithSpecialization());
+      const caregoriesWithVacancies = await getCategoriesWithVacancies();
+      this.midlle = caregoriesWithVacancies;
+
+      this.adaptCaregoriesWithVacancies = Object.freeze(adaptProf(caregoriesWithVacancies));
+      console.log(this.adaptCaregoriesWithVacancies)
+    },
+  };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- server/index.js
+++ server/index.js
@@ -1,24 +1,24 @@
-const express              = require('express');
-const appRoot              = require('app-root-path');
-const listEndpoints        = require('express-list-endpoints');
+const express = require('express');
+const appRoot = require('app-root-path');
+const listEndpoints = require('express-list-endpoints');
 const dotenvParseVariables = require('dotenv-parse-variables');
-const dotenv               = require('dotenv');
-const mongoose             = require(appRoot + '/db/connection');
-const CategoryRoutes       = require('./routers/categories');
-const VacancyRoutes        = require('./routers/vacantions');
-const corss                = require('./middleware/cross');
+const dotenv = require('dotenv');
+
+const mongoose = require(`${appRoot}/db/connection`);
+const CategoryRoutes = require('./routers/categories');
+const VacancyRoutes = require('./routers/vacantions');
+const corss = require('./middleware/cross');
 
 let env = dotenv.config({});
 env = dotenvParseVariables(env.parsed);
-const app  = express();
+const app = express();
 
 app.listen(env.EXPRESS_PORT);
 
 app.use(corss);
 
-app.use(express.static(appRoot + '/public'));
+app.use(express.static(`${appRoot}/public`));
 app.use('/category', CategoryRoutes);
 app.use('/vacancy', VacancyRoutes);
 
 console.log(listEndpoints(app));
-
