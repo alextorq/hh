eb9ceeeb270b7a05c3622ec85a74bb0607cca846
alex <xxxxx_07@inbox.ru>
2020-07-07 22:50:21 +0300

    refactoring

--- crawl/connection.js
+++ crawl/connection.js
@@ -2,7 +2,7 @@ const appRoot = require('app-root-path');
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.set('debug', true);
+mongoose.set('debug', false);
 
 mongoose.connect(`${process.env.MONGO_URL}/${process.env.MONGO_DB_PARSE}`, { useNewUrlParser: true, autoIndex: false, useUnifiedTopology: true }, (err) => {
   if (err) return console.log(err);
